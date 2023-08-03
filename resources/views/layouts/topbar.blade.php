1abb4c585f110a2ba7835b400ca454d0da914c7f
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-08-03 19:16:57 +0200

    Feat: Add top header to show guidance information

---
+++ resources/views/layouts/topbar.blade.php
@@ -0,0 +1,9 @@
+<div class="api">
+    <div class="container" style="align-items: center; justify-content: center; text-align: center;">
+        This challenge uses or needs those 
+        <a target="_blank" href="#">Exchange Rate</a>,
+        <a target="_blank" href="https://home.openweathermap.org/users/sign_up">OpenWeather</a>, and
+        <a target="_blank" href="http://api.worldbank.org/v2/indicator">World Bank</a> API keys to run. And you can get yours by clicking on the
+        eachother.
+    </div>
+</div>
