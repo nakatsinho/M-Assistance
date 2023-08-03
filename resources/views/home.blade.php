e575cf216a16d528b07a81beea64047b73e65c7f
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-08-03 19:18:47 +0200

    Feat: Improving the blade view to show search input

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+<section class="top-banner">
+    <div class="container">
+        <div class="heading-wrapper">
+            <h1 class="heading">Welcome to Mozambique Atlas</h1>
+            <form>
+                <input type="text" class="input-grey-rounded" placeholder="Search for the City" autofocus>
+                <div style="padding: 2%;">
+                    <button type="submit">Search Now</button>
                 </div>
+            </form>
+            <div class="error-message-container" id="error-container">
+                <div class="error-message" id="error-message"></div>
             </div>
         </div>
     </div>
-</div>
+</section>
+<section class="ajax-section">
+    <div class="container" style="text-align: center;">
+        <ul class="cities"></ul>
+    </div>
+</section>
 @endsection
