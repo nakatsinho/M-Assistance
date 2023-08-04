9ed59ea720caeb7212235a10a124b66f4642ab2c
Kelton M. Cumbe <nakatsinho@gmail.com>
2023-08-04 13:12:32 +0200

    Fix: Add token that is used in all possible forms

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
     <title>M-Assistance | Weather</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
 <body>
